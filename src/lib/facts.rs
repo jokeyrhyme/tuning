ed7cc3e09bc87d4f0fde11dedda9f3acd72f7219
Ron Waldon <jokeyrhyme@gmail.com>
2020-03-11 07:48:04 +1100

    switch from askama to tera (better fit)

--- src/lib/facts.rs
+++ src/lib/facts.rs
@@ -1,5 +1,8 @@
 #![deny(clippy::all)]
 
+use serde::Serialize;
+
+#[derive(Serialize)]
 pub struct Facts {}
 impl Default for Facts {
     fn default() -> Self {
--- src/lib/template.rs
+++ src/lib/template.rs
@@ -2,6 +2,7 @@
 
 use std::convert::TryFrom;
 
+use tera::{self, Context, Tera};
 use thiserror::Error as ThisError;
 
 use super::{
@@ -11,21 +12,27 @@ use super::{
 
 #[derive(Debug, ThisError)]
 pub enum Error {
+    #[error("unable to prepare template context: {}", source)]
+    Context { source: tera::Error },
     #[error(transparent)]
     Job {
         #[from]
         source: jobs::Error,
     },
+    #[error("unable to render template: {}", source)]
+    Render { source: tera::Error },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-pub fn render<S>(input: S, _facts: &Facts) -> Result<String>
+pub fn render<S>(input: S, facts: &Facts) -> Result<String>
 where
     S: AsRef<str>,
 {
     Main::try_from(input.as_ref())?; // check that we have valid TOML first
-    Ok(input.as_ref().to_string())
+
+    let context = Context::from_serialize(facts).map_err(|e| Error::Context { source: e })?;
+    Tera::one_off(input.as_ref(), &context, false).map_err(|e| Error::Render { source: e })
 }
 
 #[cfg(test)]
@@ -54,13 +61,24 @@ mod tests {
     }
 
     #[test]
+    fn render_toml_with_missing_value() {
+        let input = r#"
+            [[jobs]]
+            type = "command"
+            command = "{{ missing_value }}"
+            "#;
+        let facts = Facts::default();
+        let got = render(input, &facts);
+        assert!(got.is_err());
+        // TODO: assert on error contents
+    }
+
+    #[test]
     fn render_toml_without_expressions() {
         let input = r#"
             [[jobs]]
-            name = "run something"
             type = "command"
             command = "something"
-            argv = [ "foo" ]
             "#;
         let facts = Facts::default();
         let want = String::from(input);
